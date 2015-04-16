2f9a7ee85105241a83d2ab785fdfd8e56b88ea71
Brandon Espinoza Matarrita <bespinoza@carlosroman-hp.cetav.local>
2015-04-15 19:38:10 -0600

    cambios realizados trabajos y css

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 $(document).ready(function(){
-	$.ajax({
-		type: "GET",
-		url: 'js/trabajos.json',
-		dataType: "json",
-   		success:function(data){
-			mostrarTrabajos(data.works);
-			console.log(data);
-					
-		},
-		error:function(jqXHR, textStatus, errorThrown) {
-			console.log("Text Status:" + textStatus + "\nError:" +errorThrown);
-		}
-	});
+		$.ajax({
+				type: "GET",
+				url: 'js/trabajos.json',
+				dataType: "json",
+		   		success:function(data){
+					mostrarTrabajos(data.trabajos);
+					console.log(data);
+							
+				},
+				error:function(jqXHR, textStatus, errorThrown) {
+					console.log("Text Status:" + textStatus + "\nError:" +errorThrown);
+				}
+		});
 
-	function mostrarTrabajos(lista){
-		var showWorks = "";
-		for (var i = 0; i < lista.length; i++){
-			
-		}
-		document.getElementById("").innerHTML = showWorks;
-	}	
-
-
+		function mostrarTrabajos(lista){
+			var content = "";
+			for (var i = 0; i < lista.length; i++){
+				content += '<div class="work-items">' +
+			'<a class="works-link" target="_blank" href="' + lista[i].enlace + '">' +
+			'<img class="work-img" src='+ lista[i].img + '>' + '</a>' +
+			'<h4 class="titles-works">' + lista[i].nombre + '</h4>' +
+			'<p class="category-works">' + lista[i].category + '</p>' +
+			'<p class="description-works">' + lista[i].description + '</p>' +
+			'</div>'
+			}
+			document.getElementById("works-container").innerHTML = content;
+		}	
 });
 
